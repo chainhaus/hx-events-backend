92ef50f475b676ad18e205bfe3df77e7ea60f892
Rahil Husain <rahilhusain55@gmail.com>
2020-08-12 20:53:47 +0530

    circular dependency issue

--- src/main/java/com/rahilhusain/hxevent/service/impl/MailServiceImpl.java
+++ src/main/java/com/rahilhusain/hxevent/service/impl/MailServiceImpl.java
@@ -9,6 +9,7 @@ import com.rahilhusain.hxevent.service.SettingsService;
 import lombok.SneakyThrows;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
@@ -29,19 +30,16 @@ import static javax.mail.Message.RecipientType;
 @Service
 public class MailServiceImpl implements MailService, ApplicationContextAware {
 
-    private final JavaMailSender mailSender;
-    private final MailRepo mailRepo;
-    private final SettingsService settingsService;
+    @Autowired
+    private JavaMailSender mailSender;
+    @Autowired
+    private MailRepo mailRepo;
+    @Autowired
+    private SettingsService settingsService;
     private RsvpService rsvpService;
 
     @Value("${hx-events.app.mail.from-addr}")
     private String fromMailAddr;
-
-    public MailServiceImpl(JavaMailSender mailSender, MailRepo mailRepo, SettingsService settingsService) {
-        this.mailSender = mailSender;
-        this.mailRepo = mailRepo;
-        this.settingsService = settingsService;
-    }
 
     @SneakyThrows
     @Override
